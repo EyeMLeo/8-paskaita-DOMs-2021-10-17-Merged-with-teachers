fa1b27c284e94b7bcc9d267f513c7262a452e7fc
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-19 12:59:24 +0200

    Doms Merged with teachers files

---
+++ practice/js/uzd.js
@@ -0,0 +1,26 @@
+'use strict';
+console.log('uzd.js');
+
+/*  
+// prisideti elementam id, class, ir pan pagal poreiki
+// panaudoti tiek getElementById tiek querySelector() ar querySelectorAll()
+
+1. pakeisti h1 tektso spalva i tomato <h1>Welcome</h1>
+
+2. padaryti kad fonto storis butu normalus  <h2>Some title</h2>
+
+3. <p>Lorem ipsum dolor si. Irasyti i ji teksta "man pavyko dinamiskai atnaujinti teksta"
+
+4. <li>two</li> padidinti srifto dyti iki 34px
+
+5. paspaudus mygtuka <button>click me</button> pakeisti h1 teksta i 'Pakeiciau su mygtuku'
+
+6. <p class="time">12:45</p> elementui pakeisti teksta i dabartini laika pvz '13:50' ir padaryti pastorinta srifta
+
+7. <li>one</li> istrinti
+
+8. paspaudus <button>make article dark</button> padaryti <article> fona juoda, o teksta balta
+
+9. paspaudus <button>delete first li from list</button> istrinti pirma li el is <ol>
+
+*/
